7b3465455e7436518ae2f49011e66658cfd00488
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-03-03 17:21:11 +1300

    Asking user to input first number for calculations

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
-#importing math module to gain access to various mathematical functions and constants
+# importing math module to gain access to various mathematical functions and constants
 import math
 
+# start an infinite loop to keep calculator running & get the first number from the user (integer)
+while True:
+    input1 = int(input("What is the first number?"))
 
 
